c99617ddc61fd59b9096f1c27604742f1877880a
maximvarentsov <admin@phpcode.us>
2014-07-25 16:41:08 +0400

    fix bug

--- src/main/java/ru/gtncraft/xpconomy/XPConomy.java
+++ src/main/java/ru/gtncraft/xpconomy/XPConomy.java
@@ -6,12 +6,20 @@ import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.enchantment.EnchantItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.IOException;
 
 public final class XPConomy extends JavaPlugin implements Listener {
+
+    /*private ProtocolManager protocolManager;
+
+    @Override
+    public void onLoad() {
+        protocolManager = ProtocolLibrary.getProtocolManager();
+    }*/
 
     @Override
     public void onEnable() {
@@ -23,10 +31,36 @@ public final class XPConomy extends JavaPlugin implements Listener {
     public void onPlayerJoin(final PlayerJoinEvent event) {
         Player player = event.getPlayer();
         try {
-            player.setTotalExperience(get(player));
+            int exp = get(player);
+            if (exp != player.getTotalExperience()) {
+                SetExpFix.setTotalExperience(player, exp);
+            }
+            //updateBar(player, player.getTotalExperience());
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+    }
+
+    /*void updateBar(final Player player, float value) {
+        PacketContainer fakeLVLBar = protocolManager.createPacket(PacketType.Play.Server.EXPERIENCE);
+        //fakeLVLBar.getFloat().write(0, 0F);
+        fakeLVLBar.getIntegers().write(0, 100);
+        //fakeLVLBar.getShorts().write(0, (short) 0).write(1, (short) 0);
+        try {
+            protocolManager.sendServerPacket(player, fakeLVLBar);
+        } catch (InvocationTargetException ex) {
+            throw new RuntimeException("Cannot send packet " + fakeLVLBar, ex);
+        }
+    }*/
+
+    @EventHandler(ignoreCancelled = true)
+    @SuppressWarnings("unused")
+    public void onEnchantItem(final EnchantItemEvent event) {
+        int newLevel = event.getEnchanter().getLevel() - event.getExpLevelCost();
+        int newTotal = SetExpFix.getExpToLevel( newLevel );
+        newTotal += Math.floor(event.getEnchanter().getExp() * SetExpFix.getExpAtLevel( newLevel ));
+        SetExpFix.setTotalExperience(event.getEnchanter(), newTotal);
+        event.setExpLevelCost(0);
     }
 
     @Deprecated
@@ -37,7 +71,7 @@ public final class XPConomy extends JavaPlugin implements Listener {
 
     public void set(final OfflinePlayer player, int value) throws IOException {
         if (player.isOnline()) {
-            ((Player) player).setTotalExperience(value);
+            SetExpFix.setTotalExperience(((Player) player), value);
         } else {
             write(player, value);
         }
